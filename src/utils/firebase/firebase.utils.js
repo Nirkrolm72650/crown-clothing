04dca511de602d2f6f91488d2771e8c3b3276e05
Nirkrolm72650 <dev.brandonguyon@gmail.com>
2022-12-06 18:46:18 +0100

    First Commit

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,55 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDSlXKyazyMyOlp0WNQrkhqkhbyGRRv2eM",
+    authDomain: "crown-clothing-db-5a9b0.firebaseapp.com",
+    projectId: "crown-clothing-db-5a9b0",
+    storageBucket: "crown-clothing-db-5a9b0.appspot.com",
+    messagingSenderId: "521588444204",
+    appId: "1:521588444204:web:2f413d2e023136e0ed5589"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+   prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
+
+// Création de la base de données
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+
+    //Si les données utilisateurs n'existent pas
+    if(!userSnapshot.exists()){
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        }catch(error){
+            console.log("Erreur lors de la création de l'utilisateur", error.message);
+        }
+    }
+    // Verifier si les données utilisateurs existent
+
+    // return userDocRef
+    return userDocRef;
+};
