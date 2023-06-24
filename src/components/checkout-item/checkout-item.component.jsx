441307e89a9158b148c83b2cb28475dbf2095e3c
Nirkrolm72650 <dev.brandonguyon@gmail.com>
2023-06-24 12:55:37 +0200

    Add categories

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -6,14 +6,14 @@ import "./checkout-item.styles.scss";
 
 const CheckoutItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
+
   const { clearItemFromCart, addItemToCart, removeItemToCart } =
     useContext(CartContext);
 
   const clearItemHandler = () => clearItemFromCart(cartItem);
-
   const addItemHandler = () => addItemToCart(cartItem);
-
   const removeItemHandler = () => removeItemToCart(cartItem);
+
   return (
     <div className="checkout-item-container">
       <div className="image-container">
@@ -21,7 +21,7 @@ const CheckoutItem = ({ cartItem }) => {
       </div>
       <span className="name"> {name} </span>
       <span className="quantity">
-        <div className="arrow" onClick={() => removeItemToCart(cartItem)}>
+        <div className="arrow" onClick={removeItemHandler}>
           &#10094;
         </div>
         <span className="value">{quantity}</span>
@@ -29,7 +29,7 @@ const CheckoutItem = ({ cartItem }) => {
           &#10095;
         </div>
       </span>
-      <span className="price"> {price} $</span>
+      <span className="price"> {price}</span>
       <div className="remove-button" onClick={clearItemHandler}>
         &#10005;
       </div>
